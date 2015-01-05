c1cc4056467244c7efc17cef146a79d70dfb512f
Arana Juan <juanpablo.arana@ta.telecom.com.ar>
2015-01-05 12:14:54 -0300

    Grupo y status en vista detalles tkt

--- actions/show/open.php
+++ actions/show/open.php
@@ -2,13 +2,21 @@
 
 $T = $obCI->get_object('TEAM', $this->get_prop("valoraccion"));
 if($T){
-    $asignado = " // Asignado a: ".$T->get_prop("nombre");
+    $TT = $obCI->get_object('TKT', $this->get_prop("idtkt"));;
+    if($TT){
+        $sv=$TT->get_status();
+        $status=$sv[1];
+    }else{
+        $status=""; 
+    }
+    
+    $asignado = " // Asignado a: ".$T->get_prop("nombre")." ($status)";
 }else{
     $asignado= " // Asignado a: Indeterminado - Error ".$this->get_prop("valoraccion");
 }
 
 if($this->get_prop("UA_o")){
-    $userGen="Generado por ".$this->get_prop("UA_o")->get_prop("nombre");
+    $userGen="Generado por ".$this->get_prop("UA_o")->get_prop("nombre")."( ".$this->get_prop("UA_o")->get_prop("equiposname").")";
 }else{
     $userGen="Generado por: Indeterminado - Error ".$this->get_prop("UA");
 }

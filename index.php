308cebbec38d595b0d3accb4063a10dad07a6bf2
Arana Juan <arana.juanpablo@gmail.com>
2015-11-13 14:55:20 -0300

    Routing y fix para unix

--- index.php
+++ index.php
@@ -13,6 +13,12 @@ $Context->getLogger($Context->get_GlobalConfig()->getString('debug/log_level'));
 if ($Context->get_GlobalConfig()->getString('database/motor') == 'mssql')
     header('Content-Type: text/html; charset=iso-8859-1');
 
+/* Routing */
+if(preg_match('/\\/aux\\/.*/' ,$_SERVER["REQUEST_URI"])){
+    /* aux connections */
+    
+}
+
 $RQ = trim(file_get_contents('php://input'));
 
 $Context->request($RQ, $_SERVER['REMOTE_ADDR'], $_SERVER['REQUEST_TIME']);
